6991ab4f551dd49343bae564ead5734c22666662
JonasFuchss <jonas@fuchss.de>
2025-04-03 12:58:22 +0200

    Auto-Spawn der ersten Welle funktioniert.

--- Scripts/root.gd
+++ Scripts/root.gd
@@ -1,58 +1,44 @@
 extends Node2D
 
 
-#func _on_nn_controller_create_doodle(Doodle: PackedScene) -> void:
-	#var spawned_doodle: CharacterBody2D = Doodle.instantiate()
-	#add_child(spawned_doodle)
-	#print("... spawned doodle")
-
-var gen_count = 3
-var pop_count = 10
-
-signal create_doodle(doodle)
-
-var Doodle = preload("res://Scenes/doodle.tscn")
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			# create first generation & let it run
-			for p in pop_count:
-				print("emitting signal...")
-				create_doodle.emit(Doodle)
-
-
-@onready var width := get_viewport_rect().size.x
-@onready var height := get_viewport_rect().size.y
-
 var platform = preload("res://Scenes/Platform.tscn")
 var platformCount = 5
 var platforms = []
 var scrollSpeed = 0.05
 
+@onready var width := get_viewport_rect().size.x
+@onready var height := get_viewport_rect().size.y
 @onready var player: CharacterBody2D= $doodle
+@onready var trainer: Node = $nn_trainer
 @onready var platformParent: Node2D= $Platforms
-@onready var treshold=height * 0.5
+@onready var threshold = height * 0.5
 @onready var background: Sprite2D= $"Parallax2D/Sprite2D"
 
 
+signal level_built(x_spawn, y_spawn)
+
 func _ready()-> void:
-	player.global_position.y=treshold
+	var x_player_pos = rand_x()
+	var y_player_pos = threshold
+	
+	player.global_position.y = y_player_pos
 	for i in platformCount:
-		var inst:= platform.instantiate()
+		var inst = platform.instantiate()
 		inst.global_position.y=height/platformCount*i
 		inst.global_position.x=rand_x()
 		platformParent.add_child(inst)
 		platforms.append(inst)
-	player.global_position.x=rand_x()
-	platforms.back().global_position.x=player.global_position.x
+	# Die unterste Plattform muss unter dem Spieler sein
+	platforms.back().global_position.x = x_player_pos
+	
+	level_built.emit(x_player_pos, y_player_pos)
 	
 func rand_x()->float:
 	return randf_range(28.0, width-28.0)
 	
 func _physics_process(delta:float)-> void:
-	if player.global_position.y <treshold:
-		var move:float =lerp(0.0, treshold-player.global_position.y, scrollSpeed)
+	if player.global_position.y < threshold:
+		var move: float = lerp(0.0, threshold - player.global_position.y, scrollSpeed)
 		move_background(move)
 		player.global_position.y+=move
 		for plat in platforms:
@@ -60,11 +46,18 @@ func _physics_process(delta:float)-> void:
 			if plat.global_position.y>height:
 				plat.global_position.y -=height
 				plat.global_position.x=rand_x()
+				
 	if player.global_position.y > height:
-		game_over()	
+		game_over()
+
 func move_background(move:float)-> void:
 	var ratio :=0.75
 	background.global_position.y=fmod((background.global_position.y+height+move*ratio), height)-height
 
 func game_over()-> void:
 	get_tree().reload_current_scene()
+
+func _on_nn_trainer_create_doodle(Doodle: PackedScene, x: float, y: float) -> void:
+	var doodle: CharacterBody2D = Doodle.instantiate()
+	trainer.add_child(doodle)
+	doodle.translate(Vector2(x, y))
